5791729bbbd1ee0ff2564704e56c9fa92b115b63
Sum Tsui <sumtsui@gmail.com>
2023-01-12 10:51:13 +0800

    finally can run migrations

--- src/migrations/1670860477486-fuck-you-again.ts
+++ src/migrations/1670860477486-fuck-you-again.ts
@@ -7,12 +7,12 @@ export class fuckYouAgain1670860477486 implements MigrationInterface {
         await queryRunner.query(`
             CREATE TABLE ingredients (
                 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-                fullname VARCHAR ( 255 ) NOT NULL
-                gender VARCHAR ( 6 ) NOT NULL 
-                phone VARCHAR ( 6 ) UNIQUE NOT NULL  
-                age int NOT NULL
-                created_at DATE NOT NULL DEFAULT CURRENT_DATE 
-                updated_At DATE NOT NULL DEFAULT CURRENT_DATE 
+                fullname VARCHAR ( 255 ) NOT NULL,
+                gender VARCHAR ( 6 ) NOT NULL,
+                phone VARCHAR ( 6 ) UNIQUE NOT NULL,
+                age int NOT NULL,
+                created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+                updated_At DATE NOT NULL DEFAULT CURRENT_DATE
             );
         `)
     }
