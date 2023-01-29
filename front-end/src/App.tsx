90903ab69c83a6fef23aef149c8055d5578c8cc6
Sum Tsui <suxu>
2023-01-29 08:44:46 +0800

    add router and formik

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProfileForm from "./profile-form";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/profile-form",
+    element: <ProfileForm />,
+  },
+]);
 
 function App() {
-  return <div className="App">hahah</div>;
+  return (
+    <div className="App">
+      hahah
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
