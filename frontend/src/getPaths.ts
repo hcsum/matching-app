ee62867a8a7a54a54556ee49b2a0b0ce0e7346b2
Sum Tsui <sumtsui@gmail.com>
2023-03-01 00:04:40 +0800

    dev: update path

--- frontend/src/getPaths.ts
+++ frontend/src/getPaths.ts
@@ -1,9 +1,11 @@
 const Paths = {
   home: (eventId = ":eventId") => `/matching-event/${eventId}`,
   profile: (eventId = ":eventId", userId = ":userId") =>
-    `${Paths.home(eventId)}/profile-form/${userId}`,
+    `${Paths.home(eventId)}/user/${userId}/profile-form`,
   bio: (eventId = ":eventId", userId = ":userId") =>
     `${Paths.home(eventId)}/user/${userId}/bio-form`,
+  photo: (eventId = ":eventId", userId = ":userId") =>
+    `${Paths.home(eventId)}/user/${userId}/upload-photo`,
   userHome: (userId = ":userId") => `/user/${userId}`,
 };
 
