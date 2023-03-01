cfea2098459c07882b745ad7522a048d3a149b8b
kailiu <kaigregliu@gmail.com>
2023-03-01 16:39:35 +0800

    recover migration files

--- src/domain/photo/model.ts
+++ src/domain/photo/model.ts
@@ -1,28 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm"
-import { User } from "../user/model"
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "../user/model";
 
-
-export type PhotoInitParams = Pick<
-  Photo,
-  "user"| "url"
-  >
+export type PhotoInitParams = Pick<Photo, "user" | "url">;
 @Entity()
 export class Photo {
-    static init({url, user}:PhotoInitParams): Photo {
-        const photo = new Photo();
-        photo.url = url;
-        photo.user = user;
-        return photo;
-      }
-      
-    @PrimaryGeneratedColumn('uuid')
-    id: number
+  static init({ url, user }: PhotoInitParams): Photo {
+    const photo = new Photo();
+    photo.url = url;
+    photo.user = user;
+    return photo;
+  }
 
-    @Column()
-    url: string
+  @PrimaryGeneratedColumn("uuid")
+  id: number;
 
-    @ManyToOne(() => User, (user) => user.photos)
-    user: User
+  @Column()
+  url: string;
 
- 
+  @ManyToOne(() => User, (user) => user.photos)
+  user: User;
 }
