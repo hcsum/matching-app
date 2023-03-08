9c6929ccc4c42650444d51f7fc29b7eeaa3e1179
Sum Tsui <suxu>
2023-03-08 23:48:43 +0800

    feat: get all matching events by user id

--- server/src/controller/matching-event.ts
+++ server/src/controller/matching-event.ts
@@ -1,9 +1,16 @@
 import { RequestHandler } from "express";
 import MatchingEventRepository from "../domain/matching-event/repo";
 
-export const getMatchingEvent: RequestHandler = async (req, res) => {
+export const getMatchingEventById: RequestHandler = async (req, res) => {
   const event = await MatchingEventRepository.findOneBy({
-    id: req.params.userId,
+    id: req.params.eventId,
   });
   res.json(event);
+};
+
+export const getMatchingEventsByUserId: RequestHandler = async (req, res) => {
+  const events = await MatchingEventRepository.getMatchingEventsByUserId({
+    userId: req.params.userId,
+  });
+  res.json(events);
 };
--- server/src/router/matching-event.ts
+++ server/src/router/matching-event.ts
@@ -4,8 +4,13 @@ import { MatchingEventController } from "../controller";
 const matchingEventRouter = express.Router();
 
 matchingEventRouter.get(
+  "/matching-event/user/:userId",
+  MatchingEventController.getMatchingEventsByUserId
+);
+
+matchingEventRouter.get(
   "/matching-event/:eventId",
-  MatchingEventController.getMatchingEvent
+  MatchingEventController.getMatchingEventById
 );
 
 export default matchingEventRouter;
