bdfc1e4360cdf5fd0129c6efca3854f978d5d9ef
kailiu <kaigregliu@gmail.com>
2023-03-22 19:15:30 +0800

    update eslint and prettier config

--- server/src/migrations/1678290416057-sync.ts
+++ server/src/migrations/1678290416057-sync.ts
@@ -1,19 +1,18 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class sync1678290416057 implements MigrationInterface {
-    name = 'sync1678290416057'
+  name = "sync1678290416057";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "matching_event"
             ADD "hasEnded" boolean NOT NULL DEFAULT false
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "matching_event" DROP COLUMN "hasEnded"
         `);
-    }
-
+  }
 }
--- server/src/migrations/1678590932593-sync.ts
+++ server/src/migrations/1678590932593-sync.ts
@@ -1,20 +1,19 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class sync1678590932593 implements MigrationInterface {
-    name = 'sync1678590932593'
+  name = "sync1678590932593";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "matching_event"
                 RENAME COLUMN "stage" TO "phase"
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "matching_event"
                 RENAME COLUMN "phase" TO "stage"
         `);
-    }
-
+  }
 }
