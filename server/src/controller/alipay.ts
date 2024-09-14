621bfd304f5bc29b7bb597ccaecf980db9c3b790
Sum Tsui <suxu>
2024-09-14 19:49:40 +0800

    debug

--- server/src/controller/alipay.ts
+++ server/src/controller/alipay.ts
@@ -1,27 +1,14 @@
 import { RequestHandler } from "express";
 
 export const alipayNotify: RequestHandler = async (req, res) => {
-  // const { trade_no, trade_status, total_amount, out_trade_no } = req.query;
-  // const { trade_no, trade_status, total_amount, out_trade_no } = req.body;
-  // console.log(
-  //   "alipay notify",
-  //   trade_no,
-  //   trade_status,
-  //   total_amount,
-  //   out_trade_no
-  // );
-  // console.log("alipay notify", req.res);
-  console.log("--- Incoming Request ---");
-  console.log("Method:", req.method);
-  console.log("URL:", req.url);
-  console.log("Headers:", req.headers);
-  console.log("Query:", req.query);
-  console.log("Body:", req.body);
-  console.log("------------------------");
-  console.log("Webhook body:", req.body);
-  console.log("Webhook query:", req.query);
-  console.log("Webhook params:", req.params);
-  // res.send("success");
-  res.redirect("https://taobao.com");
+  const { trade_no, trade_status, total_amount, out_trade_no } = req.body;
+  console.log(
+    "alipay notify",
+    trade_no,
+    trade_status,
+    total_amount,
+    out_trade_no
+  );
+  res.send("success");
 };
 
