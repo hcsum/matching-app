55cc46be1d7ac4589eb46fee39af07915b6fb083
Sum Tsui <suxu>
2024-09-15 16:26:49 +0800

    wip: alipay notify

--- server/src/controller/alipay.ts
+++ server/src/controller/alipay.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+import { prisma } from "../prisma";
 
 export const alipayNotify: RequestHandler = async (req, res) => {
   const { trade_no, trade_status, total_amount, out_trade_no } = req.body;
@@ -9,6 +10,31 @@ export const alipayNotify: RequestHandler = async (req, res) => {
     total_amount,
     out_trade_no
   );
+  // 2024091522001479571454150149 TRADE_SUCCESS 0.01 7a4504ae-1f49-4d31-924c-900ca301443e
   res.send("success");
+
+  const order = await prisma.order.findUnique({
+    where: { id: out_trade_no },
+  });
+
+  if (!order || total_amount !== order.amount.toString()) {
+    throw new Error(`Invalid alipay notification: ${JSON.stringify(req.body)}`);
+  }
+
+  if (trade_status !== "TRADE_SUCCESS") {
+    return;
+  }
+
+  await prisma.order.update({
+    where: { id: order.id },
+    data: { status: "PAID" },
+  });
+
+  await prisma.participant.create({
+    data: {
+      userId: order.userId,
+      matchingEventId: order.eventId,
+    },
+  });
 };
 
