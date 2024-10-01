3006b8452afaa6269ac32e573ee839f233d69e42
Sum Tsui <suxu>
2024-10-01 15:37:13 +0800

    update

--- client/src/utils/get-formatted-date-time-string.ts
+++ client/src/utils/get-formatted-date-time-string.ts
@@ -1,17 +1,30 @@
 function toChineseDateTime(isoString: string): string {
   const date = new Date(isoString);
 
-  return date.toLocaleString("zh-CN", {
+  let timeString = date.toLocaleString("zh-CN", {
     timeZone: "Asia/Shanghai",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    weekday: "long",
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
     hour12: true,
   });
+
+  const hour = date.getHours();
+  if (hour === 0) {
+    timeString = timeString.replace("上午", "凌晨");
+  } else if (hour === 12) {
+    timeString = timeString.replace("下午", "中午");
+  }
+
+  const dateString = date.toLocaleString("zh-CN", {
+    timeZone: "Asia/Shanghai",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+  });
+
+  return `${dateString} ${timeString}`;
 }
 
 export { toChineseDateTime };
